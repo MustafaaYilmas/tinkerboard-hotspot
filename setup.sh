1f26bc5680ba9cb0b9d85944009461a06cfd1840
Mustafa Yılmaz <121809092+MustafaaYilmas@users.noreply.github.com>
2023-10-28 20:20:15 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# 1. hostapd kurulum kontrolü
+# 1. hostapd setup check
 if ! command -v hostapd &> /dev/null; then
     apt-get update
     apt-get install -y hostapd
 fi
 
-# 2. hostapd için konfig dosyası
+# 2. config file for hostapd
 HOSTAPD_CONF="/etc/hostapd/hostapd.conf"
 if [ -f "$HOSTAPD_CONF" ]; then
     rm -f "$HOSTAPD_CONF"
@@ -27,34 +27,34 @@ wpa_pairwise=TKIP
 rsn_pairwise=CCMP
 EOL
 
-# 3. hostapd durdurma
+# 3. hostapd stop
 systemctl stop hostapd
 
-# 4. ufw kurulum kontrolü
+# 4. ufw setup check
 if ! command -v ufw &> /dev/null; then
     apt-get update
     apt-get install -y ufw
 fi
 
-# 5. ufw ile 5010 portunu açma
+# 5. open port 5010 with ufw
 ufw allow 5010/tcp
 
-# 6. dnsmasq kurulum kontrolü
+# 6. dnsmasq setup check
 if ! command -v dnsmasq &> /dev/null; then
     apt-get update
     apt-get install -y dnsmasq
 fi
 
-# 7. dnsmasq.conf dosyasına değerler ekleniyor
+# 7. Adding values to dnsmasq.conf
 DNSMASQ_CONF="/etc/dnsmasq.conf"
 echo "interface=wlan0" >> $DNSMASQ_CONF
 echo "dhcp-range=10.0.0.2,10.0.0.16,255.255.255.0,24h" >> $DNSMASQ_CONF
 
-# 8. port=5353 değerini ekleniyor
+# 8. adding the value port=5353
 echo "port=5353" >> $DNSMASQ_CONF
 
 #9. daemon -reload
 systemctl daemon-reload
 
-# 10. dnsmasq yeniden başlatma
+# 10. dnsmasq reboot
 systemctl restart dnsmasq
